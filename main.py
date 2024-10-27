3f8e65ae4a4db736cc832f7d4ac2940a7689d0ad
REO2248 <49685454+REO2248@users.noreply.github.com>
2024-10-27 23:01:06 +0900

    commit

--- main.py
+++ main.py
@@ -40,34 +40,75 @@ def conversion(text):
 
 beforetext=''
 lastbefore=''
+convtext=''
+convselecting=False
+convnum=0
 
 def commit():
     global beforetext
     global lastbefore
+    global convtext
+    global convselecting
+    global convnum
     if lastbefore in acor_dic.keys():
         for i in range(len(lastbefore)):
             keystroke(keyboard.Key.backspace)
+            print(55)
         input_text(acor_dic[lastbefore])
     beforetext=''
     lastbefore=''
+    convselecting=False
+    convnum=0
 
 def stroke(char=''):
     global beforetext
     global lastbefore
+    global convtext
+    global convselecting
+    global convnum
+    global commit
     backspaced=0
+    convselecting=False
+    convnum=0
     if char=='BACKSPACE':
         beforetext=beforetext[0:-1]
         backspaced=1
     else:
+        if convselecting:
+            commit()
         beforetext+=char
     bslen, viewtext=bsandinput(lastbefore,conversion(beforetext))
     for i in range(bslen-backspaced):
         keystroke(keyboard.Key.backspace)
+        print(80)
     input_text(viewtext)
     print(conversion(beforetext))
     lastbefore=conversion(beforetext)
 
-
+def convkey(shift:int=1):
+    global beforetext
+    global lastbefore
+    global convtext
+    global convselecting
+    global convnum
+    if conversion(beforetext) not in worddic:
+        return
+    if not convselecting:
+        convtext=conversion(beforetext)
+    thiswordconvs=[convtext] + worddic[convtext]
+    print(len(thiswordconvs[convnum]))
+    if len(thiswordconvs[convnum])==1:
+        keystroke(keyboard.Key.backspace)
+    for i in range(len(thiswordconvs[convnum])//2):
+        keystroke(keyboard.Key.backspace)
+        print(100)
+    convselecting=True
+    convnum+=shift
+    convnum=convnum%len(thiswordconvs)
+    input_text(thiswordconvs[convnum])
+    print('  '.join(f'>>{i}:{item}<<' if i == convnum else f'{i}:{item}' for i, item in enumerate(thiswordconvs)))
+    print(convnum,thiswordconvs[convnum])
+    lastbefore=thiswordconvs[convnum]
 
 with keyboard.Events() as events:
     for event in events:
@@ -80,18 +121,28 @@ with keyboard.Events() as events:
                 internalkeystroke-=1
                 continue
             if type(event.key)==keyboard._win32.KeyCode and event.key.char is not None and event.key.char in keytable.keys():
+                if convselecting:
+                    commit()
                 keystroke(keyboard.Key.backspace)
+                print(120)
                 stroke(keytable[event.key.char])
             elif event.key==keyboard.Key.backspace:
                 if len(beforetext)>0:
                     stroke('BACKSPACE')
                 else:
                     commit()
+            elif event.key==keyboard.Key.ctrl_r:
+                convkey(1)
+            elif convselecting and (event.key in [keyboard.Key.down,keyboard.Key.right]):
+                convkey(1)
+            elif convselecting and (event.key in [keyboard.Key.up,keyboard.Key.left]):
+                convkey(-1)
             else:
                 if event.key in [keyboard.Key.shift, keyboard.Key.shift_l, keyboard.Key.shift_r,
-                                keyboard.Key.ctrl, keyboard.Key.ctrl_l, keyboard.Key.ctrl_r]:
+                                keyboard.Key.ctrl, keyboard.Key.ctrl_l]:
                     continue
                 if type(event.key)==keyboard._win32.KeyCode and event.key.char is not None or event.key==keyboard.Key.enter or event.key==keyboard.Key.space:
                     keystroke(keyboard.Key.backspace)
+                    print(138)
                 commit()
                 keystroke(event.key)
